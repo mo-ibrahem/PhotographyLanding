36ac10eb04624f8208f59b90257dba5579110f5c
Mohamed Abdelaziz <mmody.zizo@Hotmail.com>
2022-10-25 13:11:10 +0100

    adding target blank to links

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,8 +6,8 @@ export default function Header(){
     return(
         <div className="overlay-nav">
             <div className="header-container">
-                <a href="https://skeptech.co.uk/" >COLLAB</a>
-                <a href="https://www.behance.net/ZiadHahmed">STUDIO</a>
+                <a href="https://skeptech.co.uk/" target="_blank" >COLLAB</a>
+                <a href="https://www.behance.net/ZiadHahmed" target="_blank">STUDIO</a>
             </div>
         </div>
     )
